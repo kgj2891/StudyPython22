de91bcb2343c56df9808082c0321f234c9d69430
kgj2891 <kgj2891@hanmail.net>
2022-05-02 17:44:20 +0900

    220502 기본 문법 학습완료

--- basic/datatype02.py
+++ basic/datatype02.py
@@ -7,3 +7,37 @@ you ned Python.
 And I need C#, too.
 '''
 print(multi_line)
+
+# 리스트(배열)
+b = [1,2,3,4]
+print(b)
+b.append(5) #리스트 맨 마지막에 추가
+b.insert(3, 10) #원하는 인덱스에 추가
+print(b)
+
+b.sort() #오름차순 정렬
+print(b)
+
+b.reverse() #내림차순 정렬
+print(b)
+
+b.remove(10) #원소 삭제
+print(b)
+
+print(type(b))
+
+c = (1, 2, 3, 4)
+print(c)
+# c.append # error 튜플에서는 값 추가 불가
+print(c[2])
+
+## 딕셔너리
+spiderman = {
+    'name' : '피터 파커',
+    'age' : 18,
+    'weapon' : '웹슈터',
+    'memberOfAvengers' : True
+}
+print(spiderman)
+print(spiderman['name'])
+print(spiderman['memberOfAvengers'])
