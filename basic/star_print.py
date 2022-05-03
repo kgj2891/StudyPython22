6f9d4a0d4d14cb63f684cbbdc1d16924b61b0191
kgj2891 <kgj2891@hanmail.net>
2022-05-03 11:48:01 +0900

    220503 점심전 커밋

--- basic/star_print.py
+++ basic/star_print.py
@@ -1,8 +1,11 @@
 # * 찍기
 for x in range(1,6):
-    for y in range(x, 6):
-        print('*', end = '')
-    print('')
-
+    for y in range(x, 5):
+        print(' ', end='')
+    
+    for y in range(1, x):
+        print('*', end='')
+    
+    print(' ')
 print('Hello', end=' ')
 print('World')
